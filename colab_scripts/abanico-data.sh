ee61e3c9713e0399d6544e7669f1824d820091e8
lichi <liz.sandra.bernedo@ucsp.edu.pe>
2022-10-16 10:30:10 -0500

    Update abanico-data.sh

--- colab_scripts/abanico-data.sh
+++ colab_scripts/abanico-data.sh
@@ -7,8 +7,8 @@ mkdir /content/DATA
 mkdir /content/DATA/DataSet_ConchasAbanico
 
 echo "===> Downloading ABANICO dataset"
-#id="1qJpTORrTuCETMWg3FBJeCaCrn8IVrBDh"
-id="1xrX4CoK2gCOsNJ7HjEuceiPtnbhfpugJ"
+id="1qJpTORrTuCETMWg3FBJeCaCrn8IVrBDh"
+#id="1xrX4CoK2gCOsNJ7HjEuceiPtnbhfpugJ"
 
 
 gdown --id $id
@@ -32,10 +32,10 @@ mkdir /content/drive/MyDrive/DATA/DataSet_ConchasAbanico/data/test/images
 mkdir /content/drive/MyDrive/DATA/DataSet_ConchasAbanico/data/test/masks_process
 
 echo "===> Downloading..."
-#id="1Hed8HHT2fp5AMZPagKexZamINm04ynis"
-id="1X9AwfK_Y3tSBDjK18-gl3o2ntZQEWNlc"
+id="1Hed8HHT2fp5AMZPagKexZamINm04ynis"
+#id="1X9AwfK_Y3tSBDjK18-gl3o2ntZQEWNlc"
 gdown --id $id
-filename="images_d3.zip"
+filename="images.zip"
 src="/content/${filename}"
 dst="/content/drive/MyDrive/DATA/DataSet_ConchasAbanico/data/test/images"
 mv $src ${dst}
@@ -46,10 +46,10 @@ rm "/content/drive/MyDrive/DATA/DataSet_ConchasAbanico/data/test/images/${filena
 
 
 echo "===> Downloading..."
-#id="1asBQdvI7S8J0HkP1lAGWeDry0VSOtIwI"
-id="1c-bO7xhGzIR0D9FfUs2uttp12nYOcgwK"
+id="1asBQdvI7S8J0HkP1lAGWeDry0VSOtIwI"
+#id="1c-bO7xhGzIR0D9FfUs2uttp12nYOcgwK"
 gdown --id $id
-filename="masks_process_d3.zip"
+filename="masks_process.zip"
 src="/content/${filename}"
 dst="/content/drive/MyDrive/DATA/DataSet_ConchasAbanico/data/test/masks_process"
 mv $src ${dst}
